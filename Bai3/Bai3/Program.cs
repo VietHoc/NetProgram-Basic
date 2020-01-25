02937bc808ea565d4b0be75bab413052b6a87be5
hocle@novahub.vn <>
2020-01-25 16:50:34 +0700

    Done Bai 3.

---
+++ Bai3/Bai3/Program.cs
@@ -0,0 +1,38 @@
+﻿//3. Viết chương trình nhận vào một số nguyên và trả về cách phân tích số đó ra tích của thừa số nguyên tố.
+//   Ví dụ nhập vào 600 thì cần phải trả về `2 * 2 * 2 * 3 * 5 * 5`
+
+using System;
+
+namespace Bai3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Nhap so nguyen: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} = {1}", a, phanTichThuaSoNguyenTo(a));
+        }
+
+        private static string phanTichThuaSoNguyenTo(int a)
+        {
+            int i = 2;
+            string phanTich = string.Empty;
+
+            while (a != 1)
+            {
+                if (Properties.PrimeTool.IsPrime(i) & a % i == 0)
+                {
+                    phanTich += i.ToString() + " * ";
+                    a /= i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return phanTich.Substring(0, phanTich.Length - 3);
+        }
+    }
+}
---
+++ Bai3/Bai3/Properties/PrimeTool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Bai3.Properties
+{
+    public class PrimeTool
+    {
+        public static bool IsPrime(int candidate)
+        {
+            if ((candidate & 1) == 0)
+            {
+                if (candidate == 2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            for (int i = 3; (i * i) <= candidate; i += 2)
+            {
+                if ((candidate % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return candidate != 1;
+        }
+    }
+}
