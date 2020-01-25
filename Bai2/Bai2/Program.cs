457a4013ba12cd9e357a8e24cc5e027f844ea989
hocle@novahub.vn <>
2020-01-25 16:04:05 +0700

    Done Bai 2.

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,29 @@
+﻿//2. Viết chương trình tìm tổng các chữ số của một số nguyên
+
+using System;
+
+namespace Bai2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Nhap so tu nhien: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tong cac chu so cua {0} la: {1}", a, tongChuSo(a));
+        }
+
+        private static int tongChuSo(int a)
+        {
+            int sum = 0;
+            while (a != 0)
+            {
+                sum += (a % 10);
+                a /= 10;
+            }
+            return sum;
+        }
+    }
+}
